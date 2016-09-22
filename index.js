4a803fbd9e8eb04e5a65bafb640e23a3a5f8f67c
Jeremy Desanlis <jeremy.desanlis@scality.com>
2016-09-22 09:32:10 -0700

    FT: add CMDI backend
    This commit introduces the CDMI backend. Its goal is to use a CDMI cloud
    storage with S3.

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 'use strict'; // eslint-disable-line strict
 
-require('babel-core/register')();
+require('babel-core/register')({
+    ignore: filename =>
+        !(filename.startsWith(`${__dirname}/constants.js`) ||
+        filename.startsWith(`${__dirname}/lib/`) ||
+        filename.startsWith(`${__dirname}/tests/`) ||
+        filename.startsWith(`${__dirname}/node_modules/utapi/lib/`) ||
+        filename.startsWith(`${__dirname}/node_modules/utapi/utils/`) ||
+        filename.startsWith(`${__dirname}/node_modules/utapi/router/`) ||
+        filename.startsWith(`${__dirname}/node_modules/utapi/responses/`) ||
+        filename.startsWith(`${__dirname}/node_modules/utapi/validators/`) ||
+        filename.startsWith(`${__dirname}/node_modules/utapi/handlers/`) ||
+        filename.startsWith(`${__dirname}/node_modules/cdmiclient/lib/`)),
+});
 require('./lib/server.js').default();
